0394ef9efae0bf594a8e38c46a88d48e072090e2
repreit <315808466+repreit@users.noreply.github.com>
2026-09-03 19:26:02 +0900

    Add session expiration timestamp to accounts table in schema

--- impl/backend/nodejs/src/adapters/db/postgres/schema.sql
+++ impl/backend/nodejs/src/adapters/db/postgres/schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS accounts (
     id bigint generated always as identity PRIMARY KEY,
     address text NOT NULL UNIQUE,
     session_token text UNIQUE,
+    session_expires_at timestamptz,
     history jsonb NOT NULL,
     updated_at timestamptz NOT NULL DEFAULT now()
 );
