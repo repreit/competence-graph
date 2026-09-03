eca1a4e74ae2f28e15e2c00d269a355779fd3144
repreit <315808466+repreit@users.noreply.github.com>
2026-09-03 14:30:31 +0900

    Update accounts table to enforce NOT NULL constraint on address

--- impl/backend/nodejs/src/adapters/db/postgres/schema.sql
+++ impl/backend/nodejs/src/adapters/db/postgres/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS accounts (
     id bigint generated always as identity PRIMARY KEY,
-    address text UNIQUE,
+    address text NOT NULL UNIQUE,
     history jsonb NOT NULL,
     updated_at timestamptz NOT NULL DEFAULT now()
 );
